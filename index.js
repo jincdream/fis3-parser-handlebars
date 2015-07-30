83fd6b2a5724e87dd6dd8a4f837acf1101d60dd1
Jin_C <275727449@qq.com>
2015-07-30 18:34:13 +0800

    extend data
    extend data

--- index.js
+++ index.js
@@ -3,18 +3,20 @@
 var handlebars = require('handlebars');
 var fs = require('fs')
 var path = require('path')
+var util = require('util')
 module.exports = function(content, file, opt){
 	var _dataPath = opt.dataFile
 	var dataPath = path.join(file.dirname,_dataPath)
-	console.log(file.dirname)
+
+	var _data = util._extend({charset:file.charset.replace('utf','utf-')},opt._data || {})
+
 	var template = handlebars.compile(content)
 	var data
 	try{
 		data = require(dataPath).data
 	}catch(e){
-		console.error(e)
 		data = {}
 	}
-
+	util._extend(data,_data)
   return template(data);
 };
